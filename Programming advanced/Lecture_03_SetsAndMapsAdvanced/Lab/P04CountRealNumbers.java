b31465187611d613b58c1a1b48478d63cca0e9a2
BoyanMinev <minev.ch.work@gmail.com>
2022-02-28 01:09:44 +0200

    Add some solved problems to Programming advanced/Lecture_03_SetsAndMapsAdvanced/Lab.

---
+++ Programming advanced/Lecture_03_SetsAndMapsAdvanced/Lab/P04CountRealNumbers.java
@@ -0,0 +1,27 @@
+package Lecture_03_SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P04CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double [] values = Arrays.stream(scanner.nextLine().split(" ")).mapToDouble(Double::parseDouble)
+                .toArray();
+        Map<Double, Integer> valuesWithOccurrences = new LinkedHashMap<>();
+
+        for (double value : values){
+
+            if (!valuesWithOccurrences.containsKey(value)){
+                valuesWithOccurrences.put(value, 1);
+            }else {
+                valuesWithOccurrences.put(value, valuesWithOccurrences.get(value) + 1);
+            }
+        }
+        for (Map.Entry<Double, Integer> entry : valuesWithOccurrences.entrySet()) {
+            System.out.printf("%.1f -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
---
+++ Programming advanced/Lecture_03_SetsAndMapsAdvanced/Lab/P05AverageStudentsGrades.java
@@ -0,0 +1,11 @@
+package Lecture_03_SetsAndMapsAdvanced;
+
+import java.util.Scanner;
+
+public class P05AverageStudentsGrades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
